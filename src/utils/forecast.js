81d37557dd53c6ab4b96ec1aacade8e7cbaaa6f2
Lori Stoker <kevinstoker@MacBook-Pro.local>
2019-05-15 15:50:31 -0500

    add high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,8 @@ const forecast = (latitude, longitude, callback) => {
         else {
             callback(undefined, body.daily.data[0].summary
                 + ' It is currently '+ body.currently.temperature + ' degrees out.'
-                + ' There is a ' + body.currently.precipProbability*100 + '% chance of rain.'
+                + ' There is a ' + body.currently.precipProbability*100 + '% chance of rain. High: ' 
+                + body.daily.data[0].temperatureHigh + ', Low: ' + body.daily.data[0].temperatureLow
             )
         }
     })
